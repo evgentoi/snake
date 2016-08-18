1a07c2479c53de56f68fbde38d5fab6fd6f912db
evgentoi <evgen.d216@gmail.com>
2016-08-18 13:25:13 +0300

    Snake on the move!

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -27,6 +28,20 @@ namespace Snake
 
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+
+
 
             Console.ReadLine();
         }
--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -8,10 +8,12 @@ namespace Snake
 {
     class Snake : Figure
     {
+        Direction direction;
 
         //Конструктор змейки(хвост, длина, направление)
-        public Snake(Point tail, int length, Direction direction)
+        public Snake(Point tail, int length, Direction _direction)
         {
+            direction = _direction;
             pList = new List<Point>();
             for(int i = 0; i < length; i++)
             {
@@ -20,6 +22,25 @@ namespace Snake
                 pList.Add(p);
             }
 
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
         }
     }
 }
